9687f04cf31500c541865992857f177f56533440
github username <github email address>
2018-01-18 12:07:06 -0500

    reversestring, palindrome, and reverseint solutions added

---
+++ practice.js
@@ -0,0 +1,5 @@
+// Fizzbuzz - Write a program that prints the numbers from 1 to 100. But for multiples of three print "Fizz" instead of the number
+// and for multiples of five print "Buzz". For numbers which are multiples of both three and five, print "FizzBuzz".
+
+// Fibonacci Series - A series of numbers in which each number (Fibonacci number) is the sum of the two preceding numbers.
+// The simplest is the series 1, 1, 2, 3, 5, 8, etc. (Use recursive solution.)
