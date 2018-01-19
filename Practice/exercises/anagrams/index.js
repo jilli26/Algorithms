f5efd6653f49935dcef88f92a4d68be58a4b8ebd
github username <github email address>
2018-01-19 16:57:03 -0500

    added solution for anagrams

--- Practice/exercises/anagrams/index.js
+++ Practice/exercises/anagrams/index.js
@@ -8,6 +8,48 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+
+  //first we get rid of all the spaces and punctuation
+  const modifiedStringA = stringA.replace(/[^\w]/g, "").toLowerCase()
+  const modifiedStringB = stringB.replace(/[^\w]/g, "").toLowerCase()
+
+  //iterate through each of the strings and make them objects
+  //use objects to count the occurrence of each string
+  let modifiedStringAObject = {}
+  let modifiedStringBObject = {}
+
+  for (let character of stringA) {
+    if (!modifiedStringAObject[character]) {
+      modifiedStringAObject[character] = 1
+    } else {
+      modifiedStringAObject[character]++
+    }
+  }
+
+  for (let character of stringB) {
+    if (!modifiedStringBObject[character]) {
+      modifiedStringBObject[character] = 1
+    } else {
+      modifiedStringBObject[character]++
+    }
+  }
+
+  //check if the object keys length is the same in both
+  if (Object.keys(modifiedStringAObject).length !== Object.keys(modifiedStringBObject).length) {
+    return false
+  }
+
+  //compare both string objects
+  for (let character in modifiedStringAObject) {
+    if (modifiedStringAObject[character] !== modifiedStringBObject[character]) {
+      return false
+    }
+  }
+
+  return true
+}
+
+console.log(anagrams('rail safety', 'fairy tales'));
 
 module.exports = anagrams;
