028e05b1d06e0242748fd79c7d6491252a303f7f
github username <github email address>
2018-01-19 15:59:45 -0500

    completed chunking exercise

--- Practice/exercises/chunk/index.js
+++ Practice/exercises/chunk/index.js
@@ -29,8 +29,15 @@
 // }
 
 function chunk(array, size) {
+  const chunked = []
+  let index = 0
 
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size))
+    index += size
+  }
 
+  return chunked
 }
 
 
