d1f04491387833c6a7bc7b2f20d46c4bae266601
github username <github email address>
2018-01-26 12:19:55 -0800

    added solution to vowels problem

--- Practice/exercises/vowels/index.js
+++ Practice/exercises/vowels/index.js
@@ -7,6 +7,21 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  //set count = 0
+  let count = 0
+
+  //iterate through the string and if they're vowels, add it to the count
+  for (let char of str.toLowerCase()) {
+    if (char === "a" || char === "e" || char === "i" || char === "o" || char === "u") {
+      count++
+    }
+  }
+
+  //count the number of vowels in the array and return it
+  return count
+}
+
+console.log(vowels('Why do you ask?'));
 
 module.exports = vowels;
