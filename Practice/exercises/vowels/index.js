d0c1d7273c70a06c757e311ee7386e1bc953ef5a
github username <github email address>
2018-01-26 12:31:20 -0800

    added third solution to vowels using regex

--- Practice/exercises/vowels/index.js
+++ Practice/exercises/vowels/index.js
@@ -22,20 +22,25 @@
 //   return count
 // }
 
+// function vowels(str) {
+//   //set count = 0
+//   let count = 0
+//   let checker = ['a', 'e', 'i', 'o', 'u']
+//
+//   //iterate through the string and if they're vowels, add it to the count
+//   for (let char of str.toLowerCase()) {
+//     if (checker.includes(char)) {
+//       count++
+//     }
+//   }
+//
+//   //count the number of vowels in the array and return it
+//   return count
+// }
+
 function vowels(str) {
-  //set count = 0
-  let count = 0
-  let checker = ['a', 'e', 'i', 'o', 'u']
-
-  //iterate through the string and if they're vowels, add it to the count
-  for (let char of str.toLowerCase()) {
-    if (checker.includes(char)) {
-      count++
-    }
-  }
-
-  //count the number of vowels in the array and return it
-  return count
+  const matches = str.match(/[aeiou]/gi)
+  return matches ? matches.length : 0
 }
 
 console.log(vowels('Why do you ask?'));
