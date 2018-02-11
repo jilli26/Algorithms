fa93de90780eb36f5bd0f0fdbab8406f9e87161b
github username <github email address>
2018-02-11 17:45:03 -0500

    added time in words prob + solution

--- practice.js
+++ practice.js
@@ -156,3 +156,78 @@ function leftRotation(a, d) {
 }
 
 leftRotation([1,2,3,4,5], 4)
+
+
+
+
+//time in words 
+function timeInWords(h, m) {
+  const hours = {
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+    7: 'seven',
+    8: 'eight',
+    9: 'nine',
+    10: 'ten',
+    11: 'eleven',
+    12: 'twelve'
+  }
+
+  const minutes = {
+    1:"one minute",
+    2:"two minutes",
+    3:"three minutes",
+    4:"four minutes",
+    5:"five minutes",
+    6:"six minutes",
+    7:"seven minutes",
+    8:"eight minutes",
+    9:"nine minutes",
+    10:"ten minutes",
+    11:"eleven minutes",
+    12:"twelve minutes",
+    13:"thirteen minutes",
+    14:"fourteen minutes",
+    15:"quarter",
+    16:"sixteen minutes",
+    17:"seventeen minutes",
+    18:"eighteen minutes",
+    19:"nineteen minutes",
+    20:"twenty minutes",
+    21:"twenty one minutes",
+    22:"twenty two minutes",
+    23:"twenty three minutes",
+    24:"twenty four minutes",
+    25:"twenty five minutes",
+    26:"twenty six minutes",
+    27:"twenty seven minutes",
+    28:"twenty eight minutes",
+    29:"twenty nine minutes",
+    30:"half"
+  }
+
+  //if less than or equal to thirty, use the above key value pairs to convert number into words
+  if (m < 30 && m !== 0) {
+    return minutes[m] + ' past ' + hours[h]
+
+
+  } else if (m === 30) {
+    return 'half past ' + hours[h]
+
+  //if it's equal to 0, say it's whatever o'clock
+  } else if (m === 0) {
+    return hours[h] + " o'clock"
+
+  //if greater than thirty, do the math to calculate how many minutes until the next hour
+  } else if (m > 30) {
+    return minutes[60 - m] + ' to ' + hours[h + 1]
+  }
+
+
+}
+
+timeInWords(5, 47)
